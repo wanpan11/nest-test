d5d9a083eeb191d0cb0dd7d65a1e175cdf54cdd2
Wanpan <wanpan96@163.com>
2025-11-14 17:00:20 +0800

    feat: update

---
+++ dockerfile
@@ -0,0 +1,29 @@
+# build stage
+FROM node:20 AS build-stage
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm config set registry https://registry.npmmirror.com/
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+# production stage
+FROM node:20 AS production-stage
+
+WORKDIR /app
+
+COPY --from=build-stage /app/dist /app
+COPY --from=build-stage /app/package.json /app/package.json
+
+RUN npm config set registry https://registry.npmmirror.com/
+RUN npm install --production
+RUN npm install pm2 -g
+
+EXPOSE 3000
+
+CMD ["pm2-runtime", "/app/src/main.js"]
---
+++ src-local-db/winston/MyLogger.ts
@@ -0,0 +1,29 @@
+import { LoggerService } from '@nestjs/common';
+import dayjs from 'dayjs';
+import { createLogger, Logger, LoggerOptions } from 'winston';
+
+export class MyLogger implements LoggerService {
+  private logger: Logger;
+
+  constructor(options: LoggerOptions) {
+    this.logger = createLogger(options);
+  }
+
+  log(message: string, context: string) {
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
+  }
+
+  error(message: string, context: string) {
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
+  }
+
+  warn(message: string, context: string) {
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
+  }
+}
